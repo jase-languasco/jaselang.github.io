08f560a9d3f19867c94693bad8d8c87a3c93ef00
Jase Languasco <jaselanguasco@gmail.com>
2019-05-20 07:44:17 -0400

    add conditional build and using a github token for push to master

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:jase-languasco/jaselanguasco.github.io.git master
+git push -f https://github.com/jase-languasco/jaselanguasco.github.io.git master
 
 cd -
